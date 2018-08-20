38b414fecdd0d933133191b85b76f0ac1c74782f
Christian Bell <christiantcbell@gmail.com>
2018-08-20 17:58:02 +0100

    I want to die

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+    @user
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/shownew'
+  # get 'users/create'
+  # get 'users/edit'
+  # get 'users/update'
+  # get 'users/destroy'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+      resources :bookings, only: [:new, :create, :show, :destroy] do
+        resources :reviews, only: [:new, :create, :show, :destroy]
+      end
+    end
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
